85a5bb76c07deb9961f365f95ff6975812014fb4
Ahmed-elnoman <91709806+Ahmed-elnoman@users.noreply.github.com>
2024-05-26 15:39:35 +0200

    add ui files

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,19 @@
+ <div class="offcanvas offcanvas-start" tabindex="-1" id="navOffcanvas">
+     <script>
+         const navTarget = 'target';
+         $(function() {
+             $(`.nav-${navTarget} b`).addClass('text-danger');
+         });
+     </script>
+
+     <div class="d-flex">
+         <a href="{{ route('profile.edit') }}" class="d-block p-3 flex-grow-1 border-top rounded-0 link-dark">
+             <i class="bi bi-person-circle text-warning me-2"></i>
+             <b>{{ auth()->user()->name }}</b>
+         </a>
+         <form action="{{ route('logout') }}" method="post" class="d-block p-2 border-top border-start rounded-0">
+             @csrf
+             <button type="submit" class="btn btn-outline-primary"><i class="bi bi-power text-danger"></i></button>
+         </form>
+     </div>
+ </div>
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware('auth')->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
