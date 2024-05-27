87e3310ae25510b753783faaa611425f9fdb6841
Ahmed-elnoman <91709806+Ahmed-elnoman@users.noreply.github.com>
2024-05-27 16:39:48 +0200

    add retailer structures db

---
+++ app/Models/Retailer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Retailer extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+    protected $fillable = [
+        'retailer_code',
+        'retailer_fullName',
+        'retailer_email',
+        'retailer_password',
+        'retailer_phone',
+        'retailer_company',
+        'retailer_logo',
+        'retailer_desc',
+        'retailer_website',
+        'retailer_country',
+        'retailer_province',
+        'retailer_city',
+        'retailer_address',
+        'retailer_shipAdd',
+        'retailer_currency',
+        'retailer_adv_payment',
+        'retailer_approved',
+        'retailer_approved_by',
+        'retailer_blocked',
+        'retailer_modified',
+        'retailer_created'
+    ];
+
+    public static function fetch($id = 0, $params = null, $limit = null, $lastId = null)
+    {
+        $retailers = self::join('locations', 'retailer_country', 'location_id')
+        ->join('currencies', 'retailer_currency', 'currency_id')->orderBy('retailer_created', 'DESC')->limit($limit);
+    }
+}
