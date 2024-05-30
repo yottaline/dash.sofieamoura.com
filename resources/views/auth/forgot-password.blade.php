85e773ce3ab255af21728a24d2e90ff6dc096eee
Yottaline <yottaline@gmail.com>
2024-05-30 18:05:37 +0300

    Update auth forms

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,30 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
-
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+    <div class="d-flex h-100 w-100 justify-content-center py-5">
+        <div id="cards-container">
+            <div class="card mt-5">
+                <div class="card-body">
+                    <div class="mb-4 small text-secondary">
+                        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                    </div>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    <form method="POST" action="{{ route('password.email') }}">
+                        @csrf
+                        <div>
+                            <label for="email">Email</label>
+                            <input id="email" class="form-control" type="email" name="email"
+                                value="{{ old('email') }}" required autofocus>
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        </div>
+                        <div class="d-flex align-items-center mt-4">
+                            <button type="submit" class="btn btn-outline-dark rounded-pill btn-sm mb-3 px-5">Reset
+                                password</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
+    </div>
 </x-guest-layout>
