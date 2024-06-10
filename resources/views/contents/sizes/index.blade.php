7033703973ae4b47424ba8b9cacbdfc5424b3fe3
Ahmed-elnoman <91709806+Ahmed-elnoman@users.noreply.github.com>
2024-06-10 10:02:40 +0200

    refactor size model

--- resources/views/contents/sizes/index.blade.php
+++ resources/views/contents/sizes/index.blade.php
@@ -35,10 +35,7 @@
                 <div class="card card-box">
                     <div class="card-body">
                         <div class="d-flex">
-                            <h5 class="card-title fw-semibold pt-1 me-auto mb-3 text-uppercase">
-                                <span class="loading-spinner spinner-border spinner-border-sm text-warning me-2"
-                                    role="status"></span><span>SIZES</span>
-                            </h5>
+                            <h5 class="card-title fw-semibold pt-1 me-auto mb-3 text-uppercase">SIZES</h5>
                             <div>
                                 <button type="button" class="btn btn-outline-primary btn-circle bi bi-plus-lg"
                                     ng-click="setSize(false)"></button>
@@ -84,8 +81,111 @@
                 </div>
             </div>
         </div>
+        <div class="modal fade" id="sizeForm" tabindex="-1" role="dialog" data-bs-backdrop="static"
+            data-bs-keyboard="false" aria-labelledby="sizeFormLabel">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <form method="POST" id="sizeF" action="/sizes/submit">
+                            @csrf
+                            <input ng-if="updateSize !== false" type="hidden" name="_method" value="put">
+                            <input type="hidden" name="id" id="size_id" ng-value="list[updateSize].size_id">
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="mb-3">
+                                        <label for="sizeName">
+                                            Size Name <b class="text-danger">&ast;</b></label>
+                                        <input type="text" class="form-control" name="name"
+                                            ng-value="list[updateSize].size_name" id="sizeName" />
+                                    </div>
+                                </div>
 
-        @include('contents.components.modal.sizes')
+                                <div class="col-6">
+                                    <div class="mb-3">
+                                        <label for="sizeOrder">
+                                            Size Order </label>
+                                        <input type="text" class="form-control" name="order" max="100"
+                                            ng-value="list[updateSize].size_order" id="sizeOrder" />
+                                    </div>
+                                </div>
+
+                                <div class="col-6">
+                                    <div class="form-check form-switch mb-3">
+                                        <input class="form-check-input" type="checkbox" role="switch" name="visible"
+                                            value="1" ng-checked="+list[updateSize].size_visible" id="sizeS">
+                                        <label class="form-check-label" for="sizeS">Size Status</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="modal-footer d-flex">
+                            <button type="button" class="btn btn-outline-secondary me-auto"
+                                data-bs-dismiss="modal">Close</button>
+                            <button type="submit" form="sizeF" class="btn btn-outline-primary"
+                                ng-disabled="submitting">Submit</button>
+                            <span class="spinner-border spinner-border-sm text-warning ms-2" role="status"
+                                ng-if="submitting"></span>
+                        </div>
+                    </div>
+                </div>
+                <script>
+                    $(function() {
+                        $('#sizeF').on('submit', e => e.preventDefault()).validate({
+                            rules: {
+                                order: {
+                                    digits: true,
+                                    required: true
+                                }
+                            },
+                            submitHandler: function(form) {
+                                console.log(form);
+                                var formData = new FormData(form),
+                                    action = $(form).attr('action'),
+                                    method = $(form).attr('method');
+
+                                scope.$apply(() => scope.submitting = true);
+                                $(form).find('button').prop('disabled', true);
+                                $.ajax({
+                                    url: action,
+                                    type: method,
+                                    data: formData,
+                                    processData: false,
+                                    contentType: false,
+                                }).done(function(data, textStatus, jqXHR) {
+                                    var response = JSON.parse(data);
+                                    if (response.status) {
+                                        toastr.success('Data processed successfully');
+                                        $('#sizeForm').modal('hide');
+                                        scope.$apply(() => {
+                                            scope.submitting = false;
+                                            if (scope.updateSize === false) {
+                                                scope.list.unshift(response
+                                                    .data);
+                                                categoyreClsForm()
+                                            } else {
+                                                scope.list[scope
+                                                    .updateSize] = response.data;
+                                            }
+                                        });
+                                    } else toastr.error(response.message);
+                                }).fail(function(jqXHR, textStatus, errorThrown) {
+                                    toastr.error("error");
+                                }).always(function() {
+                                    $(form).find('button').prop('disabled', false);
+                                });
+                            }
+                        });
+                    });
+
+                    function categoyreClsForm() {
+                        $('#size_id').val('');
+                        $('#sizeName').val('');
+                        $('#sizeOrder').val('');
+                    }
+                </script>
+            </div>
+        </div>
+
     </div>
 @endsection
 
@@ -102,7 +202,7 @@
                 name: ['Un visible', 'Visible'],
                 color: ['danger', 'success']
             };
-            $('.loading-spinner').hide();
+            $scope.submitting = false;
             $scope.noMore = false;
             $scope.loading = false;
             $scope.q = '';
@@ -121,7 +221,6 @@
                 if ($scope.noMore) return;
                 $scope.loading = true;
 
-                $('.loading-spinner').show();
                 var request = {
                     q: $scope.q,
                     last_id: $scope.last_id,
@@ -131,7 +230,7 @@
                 };
 
                 $.post("/sizes/load", request, function(data) {
-                    $('.loading-spinner').hide();
+
                     var ln = data.length;
                     $scope.$apply(() => {
                         $scope.loading = false;
