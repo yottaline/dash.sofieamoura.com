bd51f0f48b1f982b4011c31b58a394afd368f0b6
Yottaline <yottaline@gmail.com>
2024-06-18 03:22:25 +0300

    Email template for approved accounts

---
+++ resources/views/email_templates/account/approved.blade.php
@@ -0,0 +1,11 @@
+<h5>Hi {{ $name }},</h5>
+<br>
+<p>Thank you for signing up for Sofie Amoura B2B.</p>
+<p>Your Account has been activated</p>
+<br>
+<p>Your login details</p>
+<p>email: <i>{{ $email }}</i></p>
+<p>password: <i>{{ $password }}</i></p>
+<br>
+<p>Your sign in page</p>
+<p><a href="https://b2b.sofieamoura.com">LOG IN TO YOUR ACCOUNT</a></p>
