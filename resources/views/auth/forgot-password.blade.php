29bf83b40da6be0bd0bcdc5864fe2690d52106f9
Ahmed-elnoman <91709806+Ahmed-elnoman@users.noreply.github.com>
2024-06-27 21:13:26 +0200

    modify forget password view

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,30 +1,29 @@
 <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <div class="d-flex h-100 w-100 justify-content-center py-5">
-        <div id="cards-container">
-            <div class="card mt-5">
-                <div class="card-body">
-                    <div class="mb-4 small text-secondary">
-                        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-                    </div>
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-                        <div>
-                            <label for="email">Email</label>
-                            <input id="email" class="form-control" type="email" name="email"
-                                value="{{ old('email') }}" required autofocus>
-                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                        </div>
-                        <div class="d-flex align-items-center mt-4">
-                            <button type="submit" class="btn btn-outline-dark rounded-pill btn-sm mb-3 px-5">Reset
-                                password</button>
-                        </div>
-                    </form>
+    <div class="container">
+        <div class="card m-auto mt-5" style="max-width: 350px">
+            <div class="card-body">
+                <div class="mb-4 text-sm text-gray-600">
+                    <b>Forgot your password? No problem</b><br>
+                    <small>Enter your email address and we will email you a password reset link</small>
                 </div>
+
+                <!-- Session Status -->
+                <x-auth-session-status class="mb-4" :status="session('status')" />
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+                    <div>
+                        <label for="email">Email</label>
+                        <input id="email" class="form-control" type="email" name="email"
+                            value="{{ old('email') }}" required autofocus>
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+                    <div class="d-flex align-items-center mt-4">
+                        <button type="submit" class="btn btn-outline-dark rounded-pill btn-sm mb-3 px-5 m-auto">Reset
+                            password</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+
 </x-guest-layout>
